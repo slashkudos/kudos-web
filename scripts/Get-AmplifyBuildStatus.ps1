67dddb564b646a7cb3f615b066293916c062b4d8
Philip Gai <PhilipMGai@gmail.com>
2022-01-29 09:21:33 +0000

    Fix this script again :)

--- scripts/Get-AmplifyBuildStatus.ps1
+++ scripts/Get-AmplifyBuildStatus.ps1
@@ -17,34 +17,26 @@ Param(
     [string]$CommitId
 )
 
-$attempts = 0
-$maxAttempts = 60 * 5 # 5 minutes
-$sleep = $false
 Write-Host "Finding deployment for commit $CommitId"
 do {
-    if ($sleep -eq $true) { Start-Sleep -Seconds 1 }
     $latestJob = $(aws amplify list-jobs --app-id $AppId --branch-name $BranchName --max-items 1) | ConvertFrom-Json
     $jobSummary = $latestJob.jobSummaries[0]
     $jobCommitId = $jobSummary.commitId
-    $sleep = $true
-    $attempts += 1
-} while ($jobCommitId -ne $CommitId -and $attempts -lt $maxAttempts)
+    Start-Sleep -Seconds 1
+} while ($jobCommitId -ne $CommitId)
 
 Write-Host "Job $($jobSummary.jobId) is deploying this commit"
 Write-Host "Waiting for job to complete..." -NoNewLine
 
-$attempts = 0
-$maxAttempts = 60 * 15 # 15 minutes
-$sleep = $false
+$jobId = $jobSummary.jobId
+
 do {
-    if ($sleep -eq $true) { Start-Sleep -Seconds 1 }
-    $latestJob = $(aws amplify list-jobs --app-id $AppId --branch-name $BranchName --max-items 1) | ConvertFrom-Json
-    $jobSummary = $latestJob.jobSummaries[0]
+    $job = $(aws amplify get-job --app-id $AppId --branch-name $BranchName --job-id $jobId) | ConvertFrom-Json
+    $jobSummary = $job.job.summary
     $endTime = $jobSummary.endTime
     Write-Host "." -NoNewLine
-    $sleep = $true
-    $attempts += 1
-} while ($endTime -eq $null -and $attempts -lt $maxAttempts)
+    Start-Sleep -Seconds 1
+} while ($endTime -eq $null)
 
 Write-Host ""
 Write-Host "Job finished with status $($jobSummary.status)"
