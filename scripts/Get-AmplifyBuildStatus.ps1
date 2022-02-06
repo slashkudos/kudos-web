cd03b473d767b19b723030ceda116e8c6eeddfac
Philip Gai <PhilipMGai@gmail.com>
2022-02-06 09:07:56 +0000

    Update build status script: exit 1 if not successful

--- scripts/Get-AmplifyBuildStatus.ps1
+++ scripts/Get-AmplifyBuildStatus.ps1
@@ -7,22 +7,22 @@
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$true)]
-    [string]$AppId,
+  [Parameter(Mandatory = $true)]
+  [string]$AppId,
 
-    [Parameter(Mandatory=$true)]
-    [string]$BranchName,
+  [Parameter(Mandatory = $true)]
+  [string]$BranchName,
 
-    [Parameter(Mandatory=$true)]
-    [string]$CommitId
+  [Parameter(Mandatory = $true)]
+  [string]$CommitId
 )
 
 Write-Host "Finding deployment for commit $CommitId"
 do {
-    $latestJob = $(aws amplify list-jobs --app-id $AppId --branch-name $BranchName --max-items 1) | ConvertFrom-Json
-    $jobSummary = $latestJob.jobSummaries[0]
-    $jobCommitId = $jobSummary.commitId
-    Start-Sleep -Seconds 1
+  $latestJob = $(aws amplify list-jobs --app-id $AppId --branch-name $BranchName --max-items 1) | ConvertFrom-Json
+  $jobSummary = $latestJob.jobSummaries[0]
+  $jobCommitId = $jobSummary.commitId
+  Start-Sleep -Seconds 1
 } while ($jobCommitId -ne $CommitId)
 
 Write-Host "Job $($jobSummary.jobId) is deploying this commit"
@@ -31,12 +31,19 @@ Write-Host "Waiting for job to complete..." -NoNewLine
 $jobId = $jobSummary.jobId
 
 do {
-    $job = $(aws amplify get-job --app-id $AppId --branch-name $BranchName --job-id $jobId) | ConvertFrom-Json
-    $jobSummary = $job.job.summary
-    $endTime = $jobSummary.endTime
-    Write-Host "." -NoNewLine
-    Start-Sleep -Seconds 1
+  $job = $(aws amplify get-job --app-id $AppId --branch-name $BranchName --job-id $jobId) | ConvertFrom-Json
+  $jobSummary = $job.job.summary
+  $endTime = $jobSummary.endTime
+  Write-Host "." -NoNewLine
+  Start-Sleep -Seconds 1
 } while ($endTime -eq $null)
 
 Write-Host ""
 Write-Host "Job finished with status $($jobSummary.status)"
+
+if (-not $jobSummary.status -eq "SUCCEED") {
+  exit 1
+}
+else {
+  exit 0
+}
