ad457106698eccf404974aa9a03151e5e0da753f
Philip Gai <PhilipMGai@gmail.com>
2022-02-12 07:23:33 +0000

    Draft static feed page

---
+++ components/headerSection.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+
+interface Props extends PropsWithChildren<{}> {}
+
+export default function HeaderSection({}: Props): JSX.Element {
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Kudos Feed 🚀</h1>
+        </div>
+      </header>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faTwitter);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://kit.fontawesome.com/161043dfbb.js"
+        crossOrigin="anonymous"
+      ></Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
