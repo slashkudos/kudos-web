65ef584021af0faf2b991668ffcdb5c0fc9534f5
Philip Gai <PhilipMGai@gmail.com>
2022-03-11 21:54:49 +0000

    Fix navbar on mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
 import { Disclosure } from "@headlessui/react";
 import { PropsWithChildren } from "react";
 import { classNames } from "../services/utilities";
-import Image from "next/image";
 
 const navigation = [{ name: "Kudos", href: "/", current: true }];
 
@@ -16,7 +15,7 @@ export default function Navbar({}: Props): JSX.Element {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
-                  <div className="hidden md:block">
+                  <div className="block">
                     <div className="flex items-baseline space-x-4">
                       {navigation.map((item) => (
                         <a
@@ -36,7 +35,7 @@ export default function Navbar({}: Props): JSX.Element {
                     </div>
                   </div>
                 </div>
-                <div className="hidden md:block">
+                <div className="block">
                   <div className="ml-4 flex items-center md:ml-6">
                     <a
                       key={"about"}
@@ -55,7 +54,7 @@ export default function Navbar({}: Props): JSX.Element {
               </div>
             </div>
 
-            <Disclosure.Panel className="md:hidden">
+            <Disclosure.Panel>
               <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 {navigation.map((item) => (
                   <Disclosure.Button
