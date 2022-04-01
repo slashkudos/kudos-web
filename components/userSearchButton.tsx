1f152a2a86afb0eb018ed62a7e2467d4569de431
Philip Gai <PhilipMGai@gmail.com>
2022-04-01 04:04:13 +0000

    Update search button style

--- components/userSearchButton.tsx
+++ components/userSearchButton.tsx
@@ -30,13 +30,13 @@ export default function UserSearchButton(props: Props): JSX.Element {
 
   return (
     <>
-      <div className="flex justify-center">
-        <div className="mb-3 xl:w-96">
+      <div className="flex justify-center mt-3">
+        <div className="xl:w-96">
           <div className="input-group relative flex items-stretch w-full mb-4 rounded">
             <input
               type="search"
               className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              placeholder="User"
+              placeholder="Search..."
               aria-label="User search"
               aria-describedby="button-addon2"
               onKeyUp={(e) =>
