e7c38714e9433daa504be307f62078ce603dcb9d
Philip Gai <PhilipMGai@gmail.com>
2022-04-29 04:24:02 +0000

    Demo using componentDidMount

--- components/scrollable.tsx
+++ components/scrollable.tsx
@@ -13,48 +13,76 @@ interface Props
     onScrollBottom: () => void;
   }> {}
 
+interface State {
+  height: number;
+}
+
 // https://stackoverflow.com/a/62497293/4215732
-export default function Scrollable(props: Props): JSX.Element {
-  const [y, setY] = useState(window.scrollY);
-  const [height, setHeight] = useState(0);
-  const ref = useRef(
-    null
-  ) as unknown as React.MutableRefObject<HTMLSpanElement>;
+export default class Scrollable extends React.Component<Props, State> {
+  ref: any;
 
-  useLayoutEffect(() => {
-    if (!ref.current) {
-      return;
-    }
-    console.log("Height: " + ref.current.clientHeight);
-    setHeight(ref.current.clientHeight);
-  }, [height]);
+  constructor(props: Props) {
+    super(props);
 
-  const handleNavigation = useCallback(
-    (e) => {
-      const window = e.currentTarget;
-      if (y > window.scrollY) {
-        console.log("scrolling up");
-      } else if (y < window.scrollY) {
-        console.log("scrolling down");
-      }
-      setY(window.scrollY);
-    },
-    [y]
-  );
-
-  useEffect(() => {
-    setY(window.scrollY);
-    window.addEventListener("scroll", handleNavigation);
-
-    return () => {
-      window.removeEventListener("scroll", handleNavigation);
+    this.state = {
+      height: 0,
     };
-  }, [handleNavigation]);
+  }
 
-  return (
-    <span ref={ref}>
-      {height}
-      {props.children}
-    </span>
-  );
+  componentDidMount() {
+    const height = this.ref.clientHeight;
+    this.setState({ height });
+  }
+
+  render() {
+    return (
+      <div className="test" ref={(span) => (this.ref = span)}>
+        Size: <b>{this.state.height}px</b> but it should be 18px after the
+        render
+      </div>
+    );
+  }
+
+  // const [y, setY] = useState(window.scrollY);
+  // const [height, setHeight] = useState(0);
+  // const ref = useRef(
+  //   null
+  // ) as unknown as React.MutableRefObject<HTMLSpanElement>;
+
+  // useLayoutEffect(() => {
+  //   if (!ref.current) {
+  //     return;
+  //   }
+  //   console.log("Height: " + ref.current.clientHeight);
+  //   setHeight(ref.current.clientHeight);
+  // }, [height]);
+
+  // const handleNavigation = useCallback(
+  //   (e) => {
+  //     const window = e.currentTarget;
+  //     if (y > window.scrollY) {
+  //       console.log("scrolling up");
+  //     } else if (y < window.scrollY) {
+  //       console.log("scrolling down");
+  //     }
+  //     setY(window.scrollY);
+  //   },
+  //   [y]
+  // );
+
+  // useEffect(() => {
+  //   setY(window.scrollY);
+  //   window.addEventListener("scroll", handleNavigation);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleNavigation);
+  //   };
+  // }, [handleNavigation]);
+
+  // return (
+  //   <span ref={ref}>
+  //     {height}
+  //     {props.children}
+  //   </span>
+  // );
 }
