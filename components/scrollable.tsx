714ceb6addbb2b43adaf2472247a961f524d01fe
Philip Gai <PhilipMGai@gmail.com>
2022-04-29 05:21:46 +0000

    Simplify again

--- components/scrollable.tsx
+++ components/scrollable.tsx
@@ -2,9 +2,7 @@ import React, {
   PropsWithChildren,
   useCallback,
   useEffect,
-  useLayoutEffect,
   useRef,
-  useState,
 } from "react";
 
 interface Props
@@ -14,18 +12,17 @@ interface Props
 
 // https://stackoverflow.com/a/62497293/4215732
 export default function Scrollable(props: Props): JSX.Element {
-  const [scrollY, setScrollY] = useState(window.scrollY);
   const ref = useRef(null) as unknown as React.MutableRefObject<HTMLDivElement>;
 
   const handleNavigation = useCallback(
     (e: Event) => {
       const window = e.currentTarget as Window;
       if (!window) return;
+
       console.log("scrollY: " + window.scrollY);
       const bottomHeight = (ref.current.scrollHeight * 2) / 3;
       console.log("scrollHeight: " + bottomHeight);
 
-      setScrollY(window.scrollY);
       if (window.scrollY >= bottomHeight) {
         props.onScrollBottom();
       }
@@ -34,7 +31,6 @@ export default function Scrollable(props: Props): JSX.Element {
   );
 
   useEffect(() => {
-    setScrollY(window.scrollY);
     window.addEventListener("scroll", handleNavigation);
 
     return () => {
