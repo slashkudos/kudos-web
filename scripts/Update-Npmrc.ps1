25e4a75dfe36c7de3970332418317139bda10d18
Philip Gai <philip@slashkudos.com>
2022-06-27 01:24:59 -0500

    Add case-insensitive search (#38)
    Also, update favicon

--- scripts/Update-Npmrc.ps1
+++
@@ -1,6 +0,0 @@
-Write-Output @"
-progress=true
-email=$(git config --get user.email)
-@slashkudos:registry=https://npm.pkg.github.com
-//npm.pkg.github.com/:_authToken=$env:GITHUB_TOKEN
-"@ > ~/.npmrc
